fba6a909537e13dc1db1fd954e32a8717868e887
Neeraj Rawat <neeraj.singh.rawat@ibm.com>
2026-05-21 20:57:22 +0530

    Done code

--- main.tf
+++ main.tf
@@ -87,7 +87,7 @@ resource "aws_instance" "web_instance" {
   ami                         = var.ami_id
   instance_type               = var.instance_type
   subnet_id                   = aws_subnet.public_subnet_1.id
-  vpc_security_group_ids      = [aws_security_group.ec2_sg.id]
+  vpc_security_group_ids      = [aws_security_group.web_sg]
   associate_public_ip_address = true
 
   tags = {
